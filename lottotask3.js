a62f70825e392eaa7081ddbfa46c5f6af9a8bb6b
Thomas Shaddock <tomshaddock@googlemail.com>
2017-01-19 19:07:19 +0000

    small annotation and creation of the zip

--- lottotask3.js
+++ lottotask3.js
@@ -1,4 +1,4 @@
-var customerNumbers = 13;
+var customerNumbers = 12;
 var winningNumbers = [12, 17, 24, 37, 38, 43];
 var alertMessageWeek = "This weeks winning numbers are\n";
 var alertCustomerNumber = "\nThe customers number is\n";
@@ -6,12 +6,14 @@ var match = 0;
 
 arrLen = winningNumbers.length;
 
+//loop through array and set the flag to positive if theres a match
 for(i = 0; i <= arrLen; i++){
   if(winningNumbers[i] == customerNumbers){
     match = 1;
   }
 }
 
+//set the message based on the flag
 if(match == 1){
   message = "\nWe have a match and a winner!"
 } else if(match == 0) {
@@ -30,4 +32,4 @@ winningNumbers = winningNumbers.replace("]", "");
 //using a small regex /,/g replaces all occurances of ',' with ', '
 winningNumbers = winningNumbers.replace(/,/g, ', ');
 
-alert(alertMessageWeek + winningNumbers + "\n" + alertCustomerNumber + customerNumbers + "\n" + message);
+alert(alertMessageWeek + winningNumbers + "\n" + alertCustomerNumber + customerNumbers + "\n" + message + "\n");
